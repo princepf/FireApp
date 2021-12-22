3922979f5faf8567b9340d7143dc769cc02656c1
princepf <ss.princef@gmail.com>
2021-12-22 10:30:23 +0530

    COMMIT2212

--- lib/pages/fireapp_page_list.dart
+++ lib/pages/fireapp_page_list.dart
@@ -7,7 +7,9 @@ class Fireapp_Page_List extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Center(child: Text("FireApp")),
+      ),
       body: FutureBuilder<List<TaskModel>>(
           future: getData(),
           builder: (context, snapshot) {
@@ -17,26 +19,51 @@ class Fireapp_Page_List extends StatelessWidget {
                 list = snapshot.data!;
                 return ListView.builder(
                     itemCount: list.length,
-                    itemBuilder: (context, index) => ListTile(
-                          title: Text("${list[index].title}"),
+                    itemBuilder: (context, index) => Column(
+                          children: [
+                            ListTile(
+                              title: Text("${list[index].username}"),
+                            ),
+                            ListTile(
+                              title: Text("${list[index].email}"),
+                            ),
+                            ListTile(
+                              title: Text("${list[index].password}"),
+                            )
+                          ],
                         ));
               }
             }
             return Center(child: CircularProgressIndicator());
           }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                    title: Text(""),
+                  ));
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 
   Future<List<TaskModel>> getData() async {
     List<TaskModel> taskList = [];
     CollectionReference tasksTable =
-        FirebaseFirestore.instance.collection('Fireapp');
+        FirebaseFirestore.instance.collection('tasks');
     var tableData = await tasksTable.get();
 
     if (tableData.docs.isNotEmpty) {
       tableData.docs.forEach((element) {
         var json = element.data() as Map;
-        taskList.add(TaskModel(id: element.id, title: json["title"]));
+        taskList.add(TaskModel(
+          id: element.id,
+          username: json["username"],
+          email: json["email"],
+          password: json["password"],
+        ));
       });
     }
     return taskList;
@@ -45,7 +72,14 @@ class Fireapp_Page_List extends StatelessWidget {
 
 class TaskModel {
   final String? id;
-  final String? title;
+  final String? username;
+  final String? email;
+  final String? password;
 
-  TaskModel({this.id, this.title});
+  TaskModel({
+    this.id,
+    this.username,
+    this.email,
+    this.password,
+  });
 }
